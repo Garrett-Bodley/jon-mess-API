706ddfead8dfd2f5f338ffaf259caccf525cacd3
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-08-13 20:52:56 -0400

    Add title and artist column to songs table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_04_213905) do
+ActiveRecord::Schema.define(version: 2021_08_06_203644) do
 
   create_table "lines", force: :cascade do |t|
     t.string "text"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2021_08_04_213905) do
     t.string "url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
+    t.string "artist"
   end
 
 end
