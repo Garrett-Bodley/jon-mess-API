d294d32580497c78b893867800a06cf4469c01ef
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-08-25 07:29:46 -0400

    Debug url path for #save_urls.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,7 +2,7 @@ class SongsController < ApplicationController
 
   def save_urls
     params[:songs].each do |song|
-      Song.create(url: "https://genius.com/" + song)
+      Song.create(url: "https://genius.com" + song)
     end
   end
 
